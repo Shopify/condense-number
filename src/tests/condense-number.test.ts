744b8b127debdad3297d7ba1db64446d463d839a
Carys Mills <carys.mills@shopify.com>
2018-06-26 16:13:12 -0400

    try other image

--- src/tests/condense-number.test.ts
+++ src/tests/condense-number.test.ts
@@ -5,7 +5,7 @@ describe('condenseNumber()', () => {
     expect(condenseNumber(10, 'en')).toBe('10');
   });
 
-  it.skip('does not condense numbers when a language does not support it', () => {
+  it('does not condense numbers when a language does not support it', () => {
     expect(condenseNumber(100000, 'it')).toBe('100.000');
   });
 
@@ -17,19 +17,19 @@ describe('condenseNumber()', () => {
     expect(condenseNumber(1500000, 'de')).toBe(`1 Mio'.'`);
   });
 
-  it.skip('condenses numbers to the provided precision', () => {
+  it('condenses numbers to the provided precision', () => {
     expect(condenseNumber(1500000, 'es', 1)).toBe('1,5 M');
   });
 
-  it.skip('handles negative numbers properly', () => {
+  it('handles negative numbers properly', () => {
     expect(condenseNumber(-150000, 'ja')).toBe('-15万');
   });
 
-  it.skip('uses Intl formatting when the locale is not supported', () => {
+  it('uses Intl formatting when the locale is not supported', () => {
     expect(condenseNumber(-150000, 'IN')).toBe('-150.000');
   });
 
-  it.skip('applies precision to Intl formatting when the locale is not supported', () => {
+  it('applies precision to Intl formatting when the locale is not supported', () => {
     expect(condenseNumber(-150000, 'IN', 2)).toBe('-150.000,00');
   });
 });
